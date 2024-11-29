e797290dc846ca8cceed4faa04654a4ac0655ee0
Chase Zhang <czhang@lcs.on.ca>
2024-11-29 06:44:09 -0500

    Added tab view.

---
+++ EnhancedTodoList/Views/StatisticsView.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticsView.swift
+//  EnhancedTodoList
+//
+//  Created by Yuzhou Zhang on 2024-11-29.
+//
+
+import SwiftUI
+
+struct StatisticsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatisticsView()
+}
---
+++ EnhancedTodoList/Views/TodoListView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoListView.swift
+//  EnhancedTodoList
+//
+//  Created by Yuzhou Zhang on 2024-11-29.
+//
+
+import SwiftUI
+
+struct TodoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoListView()
+}
