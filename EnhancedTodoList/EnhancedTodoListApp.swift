ef96e5feefd087ab9240dd0df71962dbdde8ef69
Chase Zhang <czhang@lcs.on.ca>
2024-11-29 06:36:27 -0500

    Initial Commit

---
+++ EnhancedTodoList/EnhancedTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedTodoListApp.swift
+//  EnhancedTodoList
+//
+//  Created by Yuzhou Zhang on 2024-11-29.
+//
+
+import SwiftUI
+
+@main
+struct EnhancedTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
